fe0490a8b9869f1d384e02e94b09971ab5e011f1
sajidmahmud69 <sajidmahmud136@yahoo.com>
2020-12-12 16:20:58 -0500

    new gitignore file

--- rentit_react/src/App.js
+++ rentit_react/src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function App() {
     return (
-        <form>
-            
+        <form action = "http://localhost:5000/">
+            username <input type = "text" name = "username" />
+            <br></br> <br></br>
+            password <input type = "password" name = "password"/>
         </form>
     )
 }
